541ba5d8ecf0a8e5f1ce3f5a6eb9f59924ea9885
Alexander Klink <git@alech.de>
2018-12-25 18:08:09 +0100

    day9: PointedList version, speed up from 90s to 11s!

--- src/Day9.hs
+++ src/Day9.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DeriveGeneric, FlexibleInstances #-}
 module Main where
 
 import System.Environment
@@ -7,24 +7,28 @@ import Data.Maybe (fromJust)
 import Data.List
 import Control.DeepSeq
 import GHC.Generics (Generic)
+import Data.List.PointedList hiding (next, moveN, deleteRight)
+import Data.List.PointedList.Circular (next, moveN, deleteRight)
 --import Debug.Trace
 
 type Player = Integer
 type Marble = Integer
 type Score  = Integer
 
-type Circle     = [Marble]
+type Circle     = PointedList Marble
 type Scoreboard = Map Player Score
 
 data GameState = GameState {
         numPlayers :: Player
       , maxMarble  :: Marble
       , circle     :: Circle
-      , currMarble :: Marble
       , currPlayer :: Player
       , scoreboard :: Scoreboard
     }
     deriving Generic
+instance NFData (PointedList Marble) where
+   rnf pl = (_reversedPrefix pl) `deepseq` (_focus pl) `deepseq` (_suffix pl) `deepseq` ()
+
 instance NFData GameState
 
 -- input looks like this:
@@ -42,8 +46,7 @@ startState p m =
     GameState {
         numPlayers = p
       , maxMarble  = m
-      , circle     = [ 0 ]
-      , currMarble = 0
+      , circle     = singleton 0
       , currPlayer = 0
       , scoreboard = empty
     }
@@ -62,16 +65,14 @@ placeMarble :: Marble -> GameState -> GameState
 placeMarble i gs
     | (i `mod` 23) == 0  =
         gs {
-            circle     = updateCircle23 (circle gs) (currMarble gs)
-          , currMarble = updateCurrMarble23 (circle gs) (currMarble gs)
+            circle     = updateCircle23 (circle gs)
           , currPlayer = nextPlayer
-          , scoreboard = updateScoreboard (scoreboard gs) (currPlayer gs) (circle gs) i (currMarble gs)
+          , scoreboard = updateScoreboard (scoreboard gs) (currPlayer gs) (circle gs) i
         }
     -- "normal" case, update circle
     | otherwise          =
         gs {
-            circle = updateCircle (circle gs) (currMarble gs) i
-          , currMarble = i
+            circle = updateCircle (circle gs) i
           , currPlayer = nextPlayer
         }
     where
@@ -79,41 +80,25 @@ placeMarble i gs
 
 -- normal update case, add new marble in the correct position (2 to the right
 -- of the current marble)
-updateCircle :: Circle -> Marble -> Marble -> Circle
-updateCircle circle currMarble i =
-    take n circle ++ [i] ++ drop n circle
-    where
-        n = (fromJust (elemIndex currMarble circle) + 1) `mod` (length circle) + 1
+updateCircle :: Circle -> Marble -> Circle
+updateCircle circle i =
+    Data.List.PointedList.insert i (next circle)
 
 -- remove marble 7 to the "left" of the currentMarble
-updateCircle23 :: Circle -> Marble -> Circle
-updateCircle23 circle currMarble =
-    filter (/=marbleToBeRemoved) circle
-    where
-        marbleToBeRemoved = marbleNCounterClockWise circle currMarble 7 
-
--- current Marble is now the marble 6 to the "left" of currentMarble
-updateCurrMarble23 :: Circle -> Marble -> Marble
-updateCurrMarble23 circle currMarble =
-    marbleNCounterClockWise circle currMarble 6
-
--- find the marble n counterclockwise from current marble
-marbleNCounterClockWise :: Circle -> Marble -> Int -> Marble
-marbleNCounterClockWise circle currMarble n =
-    circle !! ((currIndex - n) `mod` length circle)
-    where
-        currIndex = fromJust $ elemIndex currMarble circle
+updateCircle23 :: Circle -> Circle
+updateCircle23 circle =
+    fromJust $ deleteRight $ moveN (-7) circle
 
 -- updated the scoreboard based on the marble to be placed and the marble 7
 -- to the left
-updateScoreboard :: Scoreboard -> Player -> Circle -> Marble -> Marble -> Scoreboard
-updateScoreboard scoreboard currPlayer circle i currMarble =
+updateScoreboard :: Scoreboard -> Player -> Circle -> Marble -> Scoreboard
+updateScoreboard scoreboard currPlayer circle i =
     if currPlayer `member` scoreboard then
         adjust (+ points) currPlayer scoreboard
     else
         Data.Map.insert currPlayer points scoreboard
     where
-        points = i + marbleNCounterClockWise circle currMarble 7
+        points = i + _focus (moveN (-7) circle)
 
 main :: IO ()
 main = do
