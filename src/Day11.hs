d6d471ac268a0f019a4fd86a61f60f0ee62dc6fc
Alexander Klink <git@alech.de>
2018-12-26 18:25:34 +0100

    Day 11: part 2 (slooow, could use memoization)

--- src/Day11.hs
+++ src/Day11.hs
@@ -2,6 +2,7 @@ module Main where
 
 import System.Environment
 import Data.List (maximumBy)
+import Debug.Trace
 
 type Coordinate = (Integer, Integer)
 
@@ -22,12 +23,36 @@ powerLevelForThreeByThreeSquare :: Integer -> Coordinate -> Integer
 powerLevelForThreeByThreeSquare gridSerialNumber (x, y) =
     sum $ powerLevel gridSerialNumber <$> threeByThreeSquare (x, y)
 
+powerLevelForNByNSquare :: Integer -> Integer -> Coordinate -> Integer
+powerLevelForNByNSquare n gridSerialNumber (x, y) =
+    sum $ powerLevel gridSerialNumber <$> nByNSquare n (x, y)
+
 allPossibleThreeByThreeSquaresStartCoords :: [Coordinate]
 allPossibleThreeByThreeSquaresStartCoords =
     [(x, y) | x <- [1..298], y <- [1..298]]
+
+-- create a n x n square of coordinates starting at (x, y)
+nByNSquare :: Integer -> Coordinate -> [Coordinate]
+nByNSquare n (startX, startY) =
+    [(x, y) | x <- [startX..startX+(n-1)],
+              y <- [startY..startY+(n-1)]]
+
+allPossibleNByNSquaresStartCoords :: Integer -> [Coordinate]
+allPossibleNByNSquaresStartCoords n =
+    [(x, y) | x <- [1..300-(n-1)],
+              y <- [1..300-(n-1)]]
 
 main :: IO ()
 main = do
     [f]     <- getArgs
     gridSerialNumber <- read <$> readFile f :: IO Integer
-    print $ maximumBy (\(_, a) (_, b) -> compare a b) $ (\c -> (c, powerLevelForThreeByThreeSquare gridSerialNumber c)) <$> allPossibleThreeByThreeSquaresStartCoords
+    let
+        maxNSquare n = 
+            traceShow (n, maxN) maxN
+            where
+                maxN =
+                    maximumBy (\(_, a) (_, b) -> compare a b) $
+                              (\c -> (c, powerLevelForNByNSquare n gridSerialNumber c)) <$> allPossibleNByNSquaresStartCoords n
+    print $ maxNSquare 3
+    print $ maximumBy (\(_, (_, a)) (_, (_, b)) -> compare a b) $
+        (\n -> (n, maxNSquare n)) <$> [1..300]
