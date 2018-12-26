7d2b0bfff04f5919b3e84f058847d62f23cd925c
Alexander Klink <git@alech.de>
2018-12-26 18:36:40 +0100

    Day 11 attempt with a precomputed map of power levels, does not seem faster

--- src/Day11.hs
+++ src/Day11.hs
@@ -3,6 +3,8 @@ module Main where
 import System.Environment
 import Data.List (maximumBy)
 import Debug.Trace
+import Data.Map
+import Control.DeepSeq
 
 type Coordinate = (Integer, Integer)
 
@@ -27,6 +29,13 @@ powerLevelForNByNSquare :: Integer -> Integer -> Coordinate -> Integer
 powerLevelForNByNSquare n gridSerialNumber (x, y) =
     sum $ powerLevel gridSerialNumber <$> nByNSquare n (x, y)
 
+powerLevelForNByNSquareMap :: Integer -> Map Coordinate Integer -> Coordinate -> Integer
+powerLevelForNByNSquareMap n powerlevels (x, y) =
+    sum $ lookupPowerLevel powerlevels <$> nByNSquare n (x, y)
+    where
+        lookupPowerLevel powerlevels (x, y) =
+            findWithDefault 0 (x, y) powerlevels
+
 allPossibleThreeByThreeSquaresStartCoords :: [Coordinate]
 allPossibleThreeByThreeSquaresStartCoords =
     [(x, y) | x <- [1..298], y <- [1..298]]
@@ -42,17 +51,26 @@ allPossibleNByNSquaresStartCoords n =
     [(x, y) | x <- [1..300-(n-1)],
               y <- [1..300-(n-1)]]
 
+-- let's compute power levels only once
+powerLevelMap :: Integer -> Map Coordinate Integer
+powerLevelMap gridSerialNumber =
+    fromList [((x, y), powerLevel gridSerialNumber (x, y)) |
+                x <- [1..300],
+                y <- [1..300]]
+    
+
 main :: IO ()
 main = do
     [f]     <- getArgs
     gridSerialNumber <- read <$> readFile f :: IO Integer
     let
+        powerLevels = force $ powerLevelMap gridSerialNumber
         maxNSquare n = 
             traceShow (n, maxN) maxN
             where
                 maxN =
                     maximumBy (\(_, a) (_, b) -> compare a b) $
-                              (\c -> (c, powerLevelForNByNSquare n gridSerialNumber c)) <$> allPossibleNByNSquaresStartCoords n
+                              (\c -> (c, powerLevelForNByNSquareMap n powerLevels c)) <$> allPossibleNByNSquaresStartCoords n
     print $ maxNSquare 3
     print $ maximumBy (\(_, (_, a)) (_, (_, b)) -> compare a b) $
         (\n -> (n, maxNSquare n)) <$> [1..300]
